c1c5fc11ae51b295d61c1680f3e067b70eb863d0
leonemunyao <leonemunyao@gmail.com>
2023-06-30 21:50:56 +0300

    Changes

---
+++ Main.js
@@ -0,0 +1,27 @@
+// function 
+// Fisrst generate Otp 
+// using rand() function
+
+// Store Otp generated into your DB
+
+// Catch the $OTp and email it using SMTP or sendMail function
+
+// sendMail(
+//     to: msg:
+
+//     mesg= Hi your otp is $OTp 
+
+// )
+
+// sendMail
+
+// Auth 
+
+// checks Otp keyed in= Otp in DB
+//  if (Otpkey== OtpinDb) {
+//     Login function
+    
+//  } else {
+//     OTp is wrong, PLease try again (Link)
+//     back (function)
+//  }
